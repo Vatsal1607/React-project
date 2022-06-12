2aa645ce439e98b4979262de4e2c81c510878d43
Vatsal <silentkiller8128@gmail.com>
2022-06-12 12:07:02 +0530

    youtube

---
+++ src/Component/Slidebar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+export default function Slidebar() {
+    return (
+        <div className="">
+            <ul type="none" className='pt-3 bg-gray-900 w-min h-max '>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-bars"></i></li>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-house"></i></li>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-compass"></i></li>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-bolt"></i></li>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-city"></i></li>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-play"></i></li>
+                <li><i class="fa-solid text-2xl text-white px-4 pb-10 fa-clock-rotate-left"></i></li>
+            </ul>
+            
+        </div>
+    )
+}
